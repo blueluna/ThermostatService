556bb39ac0cc993ec7513ac7da835c9760f9d479
Erik Svensson <erik.public@gmail.com>
2012-12-04 02:25:08 +0100

    Better date handling.
    HTTPConnection seems to like to be created for each request? Seems to fix problem with the web app coming back up after failure,

--- ThermostatService/webfeeder.py
+++ ThermostatService/webfeeder.py
@@ -4,19 +4,23 @@ import http
 import json
 import datetime
 import http.client
+import pytz
 
 class Webfeeder:
     def __init__(self, host):
-        self._client = http.client.HTTPConnection(host, timeout=5)
+        self._host = host
+        self._timezone = pytz.timezone('Europe/Stockholm')
 
     def send_temperature(self, id, value):
+        client = http.client.HTTPConnection(self._host, timeout=5)
         n = datetime.datetime.now().replace(microsecond=0)
+        n = self._timezone.localize(n)
         req = json.dumps({'jsonrpc': '2.0', 'id': id, 'method': 'thermostat.temperature.add', 'params': [ id, n.isoformat(sep=' '), value ] })
         print(req)
         headers = {'content_type': 'text/javascript; charset=UTF-8'}
         try:
-            self._client.request('post', '/json/', body=req, headers=headers)
-            response = self._client.getresponse()
+            client.request('post', '/json/', body=req, headers=headers)
+            response = client.getresponse()
             body = response.read()
             res = json.loads(body.decode('utf-8'))
             print(res)
